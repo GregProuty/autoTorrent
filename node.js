a7bc62c472594b3eafbac5f027ec794a34a947d7
GregoryProuty <g.prout009@gmail.com>
2016-02-25 21:25:11 -0800

    Initial

--- node.js
+++ node.js
@@ -109,6 +109,12 @@ app.post('/add_file', function (req, res) {
   });
 });
 
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on', http.address().port);
+// });
+
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
--- public/button.js
+++ public/button.js
@@ -3,32 +3,34 @@ document.getElementById("loginButton").onclick = function () {
 }
 
 document.getElementById("clickMe").onclick = function () { 
-    params = "q=" + document.getElementById("filename").value;
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+  params = "q=" + document.getElementById("filename").value;
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      console.log('success');
+      var str = '<ul>';
             
-            var str = '<ul>';
-            
-            Array.prototype.clean = function(deleteValue) {
-                for (var i = 0; i < this.length; i++) {
-                if (this[i] == deleteValue) {         
-                    this.splice(i, 1);
-                    i--;
-                }
-            }
-                return this;
-            };
-            JSON.parse(xmlhttp.responseText).clean(null).forEach(function(el){
-                str += '<li><a href="http://localhost:3000/add_to_put?q=' + el.torrentLink + '"> Title: ' + el.title + ' Seeds: ' + el.seeds + '</a></li>' ;
-            })
-            str += '</ul>'; 
-            document.getElementById("links").innerHTML = str;
-            
-            
-            
+      Array.prototype.clean = function(deleteValue) {
+        for (var i = 0; i < this.length; i++) {
+          if (this[i] == deleteValue) {         
+            this.splice(i, 1);
+            i--;
+          }
         }
-    };
-    xmlhttp.open("POST", "/add_file?q=" + document.getElementById("filename").value, true);
-    xmlhttp.send(null);
+        
+        return this;
+      };
+            
+      JSON.parse(xmlhttp.responseText).clean(null).forEach(function(el){
+        console.log('building');
+        str += '<li><a href="http://localhost:3000/add_to_put?q=' + el.torrentLink + '"> Title: ' + el.title + ' Seeds: ' + el.seeds + '</a></li>';
+      })
+      str += '</ul>'; 
+      console.log(str);
+      document.getElementById("links").innerHTML = str;       
+    }
+  }; 
+  xmlhttp.open("POST", "/add_file?" + params, true);
+  xmlhttp.send(null); 
 }
+    
