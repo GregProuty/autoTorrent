2bf94eae6fca541d0ee635e40c1ddbd2d958b82f
GregoryProuty <g.prout009@gmail.com>
2016-02-25 21:32:44 -0800

    Fix listen

--- node.js
+++ node.js
@@ -117,7 +117,7 @@ app.post('/add_file', function (req, res) {
 
 
 
-http.createServer(app).listen(app.get('port'),
+app.listen(app.get('port'),
   function(){
     console.log("Express server listening on port " + app.get('port'));
 });
