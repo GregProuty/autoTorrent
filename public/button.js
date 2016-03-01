3e11e51b539623858d9602a87063d6628aa39b58
GregoryProuty <g.prout009@gmail.com>
2016-02-29 17:12:56 -0800

    make it better

--- public/button.js
+++ public/button.js
@@ -11,9 +11,7 @@ document.getElementById("loginButton").onclick = function () {
     window.location = ('https://api.put.io/v2/oauth2/authenticate?client_id=2332&response_type=code&redirect_uri=http://autotorrent.herokuapp.com/put_oauth');
 }
 
-document.getElementById("clickMe").onclick = returnTorrents();
-
-
+document.getElementById("clickMe").onclick =
 function returnTorrents() { 
   params = "q=" + document.getElementById("filename").value;
   xmlhttp = new XMLHttpRequest();
