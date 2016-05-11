1d01d7a15e09dd38b705d5493fd8d75403d41158
GregoryProuty <g.prout009@gmail.com>
2016-05-11 00:10:50 -0700

    syle ul

--- public/index.js
+++ public/index.js
@@ -1,10 +1,8 @@
 //login button behavior
 //directs to the put.io login page
-var loggedIn = false;
 
 document.getElementById("loginButton").onclick = function () { 
     window.location = ('https://api.put.io/v2/oauth2/authenticate?client_id=2332&response_type=code&redirect_uri=http://autotorrent.herokuapp.com/put_oauth');
-    loggedIn = true;
 }
 
 //lists ten torrent links on the page
@@ -41,11 +39,9 @@ function listTorrents() {
 
 //"SEARCH" button behavior, calls listTorrents()
 document.getElementById("clickMe").onclick = function(){
-    if(loggedIn === false){
-        alert("Please log in")
-    } else {
+
         listTorrents();
-    }
+
 }
 
 //calls listTorrents on keypress enter
