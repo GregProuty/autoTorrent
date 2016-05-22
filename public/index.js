b4aad1ef80e2a75451d5c74c729a1aab890c290b
Gregory Prouty <gprout009@gmail.com>
2016-05-22 16:19:47 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -59,26 +59,24 @@ document.getElementById("filename").addEventListener("keydown", function (e) {
     }
 });
 
-function getCookie(name){
-  var dc = document.cookie;
-  var prefix = name + "=";
-  var begin = dc.indexOf("; " + prefix);
-  if(begin === -1){
-    begin = dc.indexOf(prefix);
-    if (begin != 0) {
-      console.log("returned null")
-      return null;
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
     }
-  }
-  else {
-    begin += 2;
-    var end = document.cookie.indexOf(";", begin);
-    if(end === -1){
-      end = dc.length;
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
     }
-  }
-  return unescape(dc.substring(begin + prefix.length, end));
-}
+    return unescape(dc.substring(begin + prefix.length, end));
+} 
 
 function protectedDisplay(){
   var session = getCookie("session"); 
