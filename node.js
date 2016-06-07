64cb9dd117649c0821cb4451912abfb0c295e2d0
GregoryProuty <g.prout009@gmail.com>
2016-06-07 15:42:41 -0700

    make it better

--- node.js
+++ node.js
@@ -73,7 +73,7 @@ app.get('/add_to_put', function(req, res) {
 
 //put.io api for authentication
 app.get('/put_oauth', function(req, res) {
-  var redirectUri = "http://autotorrent.herokuapp.com";
+  var redirectUri = "http://autotorrent.herokuapp.com/searh.html";
   if(process.env.DEVELOPMENT){
     redirectUri = "http://localhost:3000";
   }
