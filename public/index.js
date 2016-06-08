4b77fad2ae408b0e80ace39e486bcb42527d05de
GregoryProuty <g.prout009@gmail.com>
2016-06-08 13:32:44 -0700

    make it better

--- public/index.js
+++ public/index.js
@@ -2,7 +2,8 @@
 //directs to the put.io login page
 
 document.getElementById("loginButton").onclick = function () { 
-    window.location = ('https://api.put.io/v2/oauth2/authenticate?client_id=2332&response_type=code&redirect_uri=http://autotorrent.herokuapp.com/put_oauth');
+//     window.location = ('https://api.put.io/v2/oauth2/authenticate?client_id=2332&response_type=code&redirect_uri=http://autotorrent.herokuapp.com/put_oauth');
+       window.location = ('https://api.put.io/v2/oauth2/authenticate?client_id=2332&response_type=code&redirect_uri=http://autotorrent.herokuapp.com/search.html');
 }
 
 
