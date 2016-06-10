08b16b0f0215c9f6ab73664c1cf085fd2220037b
GregoryProuty <g.prout009@gmail.com>
2016-06-10 14:08:29 -0700

    make it better

--- public/index1.js
+++
@@ -1,9 +0,0 @@
-//login button behavior
-//directs to the put.io login page
-
-document.getElementById("loginButton").onclick = function () { 
-    window.location = ('https://api.put.io/v2/oauth2/authenticate?client_id=2332&response_type=code&redirect_uri=http://autotorrent.herokuapp.com/put_oauth');
-}
-
-
-
